84d7c128717bd2d7c88f31d26f4d03a3daae9efa
Cyrus Javan <javan.cyrus@gmail.com>
2020-12-15 22:32:14 -0800

    tf-examples

---
+++ tf-examples/example-pet/pet.tf
@@ -0,0 +1,13 @@
+provider example {}
+
+terraform {
+  required_providers {
+    example = {
+      source = "example.com/example/example"
+    }
+  }
+}
+
+resource "example_pet" "pet_1" {
+  name = "sharik"
+}
