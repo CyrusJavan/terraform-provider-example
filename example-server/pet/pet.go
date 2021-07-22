6d4dde08ad0f17550a56196fa673db318961a89a
James Mills <prologic@shortcircuit.net.au>
2021-07-22 20:51:06 +1000

    Fix import path for bitcask

--- example-server/pet/pet.go
+++ example-server/pet/pet.go
@@ -9,7 +9,7 @@ import (
 	"github.com/CyrusJavan/terraform-provider-example/example-server/models"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
-	"github.com/prologic/bitcask"
+	"git.mills.io/prologic/bitcask"
 )
 
 type PetServer struct {
--- example-server/restapi/configure_pet.go
+++ example-server/restapi/configure_pet.go
@@ -9,7 +9,7 @@ import (
 	"github.com/go-openapi/errors"
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/runtime/middleware"
-	"github.com/prologic/bitcask"
+	"git.mills.io/prologic/bitcask"
 
 	petServer "github.com/CyrusJavan/terraform-provider-example/example-server/pet"
 
